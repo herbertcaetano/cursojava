617cde2919d87a5eaf67c31b89a48de851d68d09
herbertcaetano <39100599+herbertcaetano@users.noreply.github.com>
2021-09-01 20:58:18 -0300

    arquivos iniciais

---
+++ teste/src/testemain/Main.java
@@ -0,0 +1,11 @@
+package testemain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("teste de repositorio git");
+
+	}
+
+}
