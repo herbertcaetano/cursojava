f5800655bed568945f5b3a0a2b95c9c36819ed30
Herbert Caetano <herbert.caetano@gmail.com>
2021-09-01 23:08:00 -0300

    teste de push

--- teste/src/testemain/Main.java
+++ teste/src/testemain/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("teste de repositorio git");
+		System.out.println("teste de push");
 
 	}
 
